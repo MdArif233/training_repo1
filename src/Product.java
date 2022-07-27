19189b65521e912a73c345b0cb05d5a5449abafb
MdArif233 <mdarif067787@gmail.com>
2022-07-27 16:05:26 +0530

    2nd commit

--- src/Product.java
+++ src/Product.java
@@ -3,5 +3,6 @@ public class Product
     public static void main(String[] args)
 {
     System.out.print("this is the new program in java");
+    System.out.print("thi is the updated change in the program");
 }
 }
